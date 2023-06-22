04b6ead70941a4f6e17fe6ed600a50bb2a621b36
Shekar_selva <shekar.selva90@gmail.com>
2023-06-22 08:55:23 +0530

    New commit_1

--- src/main/java/Session1/Test2_afterbranchcheckout.java
+++ src/main/java/Session1/Test2_afterbranchcheckout.java
@@ -1,5 +1,15 @@
 package Session1;
 
 public class Test2_afterbranchcheckout {
+	
+ public static void name() {
+	
+{
+	// TODO Auto-generated method stub
+	 System.out.println("Hello");
 
+
+
+}
+}
 }
